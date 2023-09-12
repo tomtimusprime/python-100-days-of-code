08b109aeb5586cb077f3dadd0b89e9a0287990ed
Tom Black <tom.black06007@gmail.com>
2023-09-12 16:02:31 -0600

    Added functionality to be able to have user interaction.

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,5 +1,3 @@
-
-
 def encrypt(inputWord, numberToShift):
     lettersInAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -24,7 +22,25 @@ def decrypt(inputWord, numberToShift):
 
     return new_word
 
-print(encrypt("helloworld", 5))
-encryptedWord = encrypt("helloworld", 5)
-print(decrypt(encryptedWord, 5))
+keepPlaying = True
+keepPlayingInput = ''
+while keepPlaying == True: 
+    inputWord = input("Input a word in all lowercase that you would like to encrypt (no spaces, please):")
+    numberOfSpaces = int(input("How many letters would you like the letters to be shifted by?"))
+    encryptedWord = encrypt(inputWord, numberOfSpaces)
+    print(f"The following is your decrypted word: {encryptedWord}")
+    print("\n")
+    yesNo = input("Would you like to have your word decrypted? (y/n)").lower()
+    if yesNo == 'y':
+        print(decrypt(encryptedWord, numberOfSpaces))
+
+    keepPlayingInput = input("Do you want encrypt another word? (y/n)").lower()
+    print(keepPlayingInput)
+    if keepPlayingInput == "n":
+        keepPlaying = False
+        
+    
+
+# encryptedWord = encrypt("trifle", 5)
+# print(decrypt(encryptedWord, 5))
 
