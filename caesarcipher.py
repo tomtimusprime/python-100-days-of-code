c39ba65e701c82789f12abef8ccfab43f2c02882
Tom Black <tom.black06007@gmail.com>
2023-09-12 12:49:33 -0600

    finished the cypher program

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,1 +1,30 @@
-print("Caesar Cipher")
+
+
+def encrypt(inputWord, numberToShift):
+    lettersInAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                          'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    new_word = ''
+    for letter in inputWord:
+        position = lettersInAlphabet.index(letter)
+        new_position = position + numberToShift
+        new_character = lettersInAlphabet[new_position]
+        new_word += new_character
+
+    return new_word
+
+def decrypt(inputWord, numberToShift):
+    lettersInAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                          'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    new_word = ''
+    for letter in inputWord:
+        position = lettersInAlphabet.index(letter)
+        new_position = position - numberToShift
+        new_character = lettersInAlphabet[new_position]
+        new_word += new_character
+
+    return new_word
+
+print(encrypt("helloworld", 5))
+encryptedWord = encrypt("helloworld", 5)
+print(decrypt(encryptedWord, 5))
+
