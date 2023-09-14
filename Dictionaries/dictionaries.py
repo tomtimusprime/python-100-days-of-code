4c61bd027090662dcccccd086b7c78a1678f98e3
Tom Black <tom.black06007@gmail.com>
2023-09-13 22:54:41 -0600

    Added folders to help keep things more organized and working on dictionaries in python

---
+++ Dictionaries/dictionaries.py
@@ -0,0 +1,6 @@
+student_grades = { "Harry": 81,
+                    "Hermione": 99,
+                    "Ron" : 78,
+                    "Draco" : 72,
+                    "Neville" : 88
+                 }
