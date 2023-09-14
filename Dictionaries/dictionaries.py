141383c5e24a93669b3e741d117729067a0469c1
Tom Black <tom.black06007@gmail.com>
2023-09-13 23:18:56 -0600

    Finished a dictionaries challenge.

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -1,6 +1,28 @@
-student_grades = { "Harry": 81,
+student_scores = { "Harry": 81,
                     "Hermione": 99,
                     "Ron" : 78,
                     "Draco" : 72,
-                    "Neville" : 88
+                    "Neville" : 68
                  }
+
+student_grades = {
+    "Harry": 81,
+    "Hermione": 99,
+    "Ron" : 78,
+    "Draco" : 72,
+    "Neville" : 88
+}
+
+for key in student_scores:
+    if student_scores[key] > 91:
+        student_grades[key] = "Outstanding"
+    elif student_scores[key] >= 81:
+        student_grades[key] = "Solid Job"
+    elif student_scores[key] >= 71:
+        student_grades[key] = "Acceptable"
+    elif student_scores[key] >= 61:
+        student_grades[key] = "Poor Performane"
+
+for key in student_grades:
+    print(key)
+    print(student_grades[key])
