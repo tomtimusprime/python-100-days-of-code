93795f2c26e5e5a4e8c4d713de4c4e4b90242923
Tom Black <tom.black06007@gmail.com>
2023-09-15 16:59:13 -0600

    Finished a simple silent auction app.

--- Silent Auction/silentauction.py
+++ Silent Auction/silentauction.py
@@ -1,9 +1,15 @@
+keepPlaying = True
 auctionBids = {}
-name = input("What is your name?")
-bid = input("What is your bid?")
+keepPlayingInput = ''
+while keepPlaying:
+    name = input("What is your name?")
+    bid = input("What is your bid?")
+    auctionBids[name] = bid
+    keepPlayingInput = input("Would you like to bid again?(y/n)").lower()
+    if keepPlayingInput == "n":
+        keepPlaying = False
 
-auctionBids[name] = bid
-
-print(auctionBids)
+Keymax = max(zip(auctionBids.values(), auctionBids.keys()))[1]
+print(f"The highest bid is {auctionBids[Keymax]} and belongs to {Keymax}")
 
 
