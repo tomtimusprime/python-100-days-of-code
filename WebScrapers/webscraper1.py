6fa6f6b59d496bf0cf040228ba4e8fbbf12d0e38
Tom Black <tom.black06007@gmail.com>
2023-09-22 12:56:37 -0600

    Added some functionality to the web scraper.

--- WebScrapers/webscraper1.py
+++ WebScrapers/webscraper1.py
@@ -3,4 +3,6 @@ from bs4 import BeautifulSoup
 
 url = 'https://12695.portal.athenahealth.com'
 response = requests.get(url)
-print(response)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.title.text.strip())
