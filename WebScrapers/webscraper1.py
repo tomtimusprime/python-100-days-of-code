831bf320cd6424d6128d47e51eb61fc4525aa272
Tom Black <tom.black06007@gmail.com>
2023-09-23 23:24:49 -0600

    web scraper project

--- WebScrapers/webscraper1.py
+++ WebScrapers/webscraper1.py
@@ -6,3 +6,4 @@ response = requests.get(url)
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
 print(soup.title.text.strip()) #the strip function takes out all the spaces and tabs.
+print("web scraper.")
