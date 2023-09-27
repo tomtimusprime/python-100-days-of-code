826458eca40b1cd09c0168e83f4126702adb851b
Tom Black <tom.black06007@gmail.com>
2023-09-27 14:49:15 -0600

    created a function to get clinic name

--- WebScrapers/webscraper1.py
+++ WebScrapers/webscraper1.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://12695.portal.athenahealth.com'
-response = requests.get(url)
-html = response.text
-soup = BeautifulSoup(html, 'html.parser')
-print(soup.title.text.strip()) #the strip function takes out all the spaces and tabs.
-print("web scraper.")
-print("web scraper.")
-print("Web scraper.")
+def get_clinic_name(clinic_id):
+    url = f'https://{clinic_id}.portal.athenahealth.com'
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    clinic_name = soup.find_all('h1')[-1].text.strip()
+    return clinic_name
+print(get_clinic_name(12695)) #the strip function takes out all the spaces and tabs.
