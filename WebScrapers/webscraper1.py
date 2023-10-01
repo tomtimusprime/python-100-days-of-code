71c0a242cc44b2c2fd51916f049feab20c848a54
Tom Black <tom.black06007@gmail.com>
2023-09-30 21:41:29 -0600

    creating a password generator.

--- WebScrapers/webscraper1.py
+++ WebScrapers/webscraper1.py
@@ -8,4 +8,5 @@ def get_clinic_name(clinic_id):
     soup = BeautifulSoup(html, 'html.parser')
     clinic_name = soup.find_all('h1')[-1].text.strip()
     return clinic_name
-print(get_clinic_name(12695)) #the strip function takes out all the spaces and tabs.
+print(get_clinic_name(8002)) #the strip function takes out all the spaces and tabs.
+# 12695
