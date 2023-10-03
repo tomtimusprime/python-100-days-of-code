35a248fdda6828838d7d8e1eb6912bd9c1776f99
Tom Black <tom.black06007@gmail.com>
2023-10-03 10:49:23 -0600

    Added a parameter to the api

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello, this is my first FastAPI app."}
+
+@app.get("/hello-world/{user_id}")
+async def hello(user_id:int):
+    return {"message": f"Hello world. This is another test get response. User Id: {user_id}"}
